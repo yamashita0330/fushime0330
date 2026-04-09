7544f06071018c1d105476a57696761868f300cc
yamashita0330 <challengym@outlook.jp>
2026-04-09 14:10:19 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -113,10 +113,8 @@ class SupabaseClient {
   }
 }
 
-const SB_URL_FIXED = (typeof process !== "undefined" && process.env?.VITE_SB_URL)
-  || "https://vcqfgkypsljgpxhsnfxv.supabase.co";
-const SB_KEY_FIXED = (typeof process !== "undefined" && process.env?.VITE_SB_KEY)
-  || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2Y3hienN4dmJvZWFwaW9pbHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyODgxMDgsImV4cCI6MjA5MDg2NDEwOH0.JI-dNBXwu2JxZZ1BvqOhwiTnwtDmfn6iQzEcN9LiZM4";
+const SB_URL_FIXED = "https://dvcxbzsxvboeapioilsg.supabase.co";
+const SB_KEY_FIXED = "sb_publishable_f6ckuJu79d_keYUMZdmVlw_f3-L3qrs";
 
 const SB_URL_KEY = "sb_url";
 const SB_KEY_KEY = "sb_key";
